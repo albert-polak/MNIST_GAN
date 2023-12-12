aa7898a44d208dbac31c7a6843e40d64b5a61cb7
albert_polak <lugii1999@gmail.com>
2023-12-12 19:25:33 +0100

    GAN module

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from torchvision import datasets, transforms
+
+import torchvision
 from torch.utils.data import DataLoader
 import lightning as L
 
@@ -64,114 +65,90 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.generator(x)
 
-# class GAN(L.LightningModule):
-#     def __init__(self, z_dim, img_channels, features_dim, lr=2e-4, batch_size=32):
-#         super().__init__()
+class GAN(L.LightningModule):
+    def __init__(self, z_dim, img_channels, features_dim, lr=2e-4, batch_size=32):
+        super().__init__()
 
-#         self.save_hyperparameters()
-#         self.automatic_optimization = False
+        self.save_hyperparameters()
+        self.automatic_optimization = False
 
-#         self.lr = lr
-#         self.batch_size = batch_size
+        self.lr = lr
+        self.batch_size = batch_size
 
-#         self.loss_fn = nn.BCELoss()
+        self.loss_fn = nn.BCELoss()
 
-#         self.generator = Generator(self.hparams.z_dim, img_channels=img_channels, features_dim=features_dim)
-#         self.discriminator = Discriminator(img_channels=img_channels, features_dim=features_dim)
+        self.generator = Generator(self.hparams.z_dim, img_channels=img_channels, features_dim=features_dim)
+        self.discriminator = Discriminator(img_channels=img_channels, features_dim=features_dim)
 
-#         self.gen_optimizer = Adam(self.parameters(), lr=self.lr)
-#         self.dis_optimizer = Adam(self.parameters(), lr=self.lr)
+        self.gen_optimizer = Adam(self.generator.parameters(), lr=self.lr)
+        self.dis_optimizer = Adam(self.discriminator.parameters(), lr=self.lr)
 
-#     def forward(self, X):
-#         mel, chroma, tonetz = X
-#         # print(mel.shape)
-#         cnn_input = np.repeat(mel[np.newaxis, ...].cpu(), 3, axis=0).cuda()
-#         cnn_input = cnn_input.permute(1, 0, 2, 3)
-#         # print(cnn_input.shape)
-#         transform = transforms.Compose([
-#            models.ResNeXt50_32X4D_Weights.IMAGENET1K_V2.transforms()
-#         ])
-#         cnn_input = transform(cnn_input)
-#         # print(cnn_input.shape)
-#         cnn = self.cnn_model(cnn_input)
-#         cnn = cnn.view(cnn.size(0), -1)
-#         chroma = chroma.to(torch.float32).cuda()
+    def forward(self, X):
+        return self.generator(X)
+    
+    # def configure_optimizers(self):
+    #         scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.90)
+    #         return {"optimizer": self.optimizer, "lr_scheduler": scheduler}
+
+    def _step(self, batch):
         
-#         # print(X.shape)
-#         # X = X.view(X.size(0), X.size(2), X.size(3))
+        imgs, _ = batch
 
-#         chroma = chroma.permute(0, 2, 1)
-#         # print(chroma.shape)
+        # sample noise
+        z = torch.randn(imgs.shape[0], self.hparams.latent_dim)
+        z = z.type_as(imgs)
 
+        # train generator
+        # generate images
+        self.toggle_optimizer(self.gen_optimizer)
+        self.generated_imgs = self(z)
 
-#         h0 = torch.randn(1, chroma.shape[0], 512).cuda()
+        # log sampled images
+        sample_imgs = self.generated_imgs[:6]
+        grid = torchvision.utils.make_grid(sample_imgs)
+        self.logger.experiment.add_image("generated_images", grid, 0)
 
-#         chroma, hn = self.gru(chroma, h0)
+        # ground truth result (ie: all fake)
+        # put on GPU because we created this tensor inside training_loop
+        valid = torch.zeros(imgs.size(0), 1)
+        valid = valid.type_as(imgs)
 
-#         chroma_output = chroma[:, -1, :]
+        # adversarial loss is binary cross-entropy
+        g_loss = self.loss_fn(self.discriminator(self(z)), valid)
+        self.log("g_loss", g_loss, prog_bar=True)
+        self.manual_backward(g_loss)
+        self.gen_optimizer.step()
+        self.gen_optimizer.zero_grad()
+        self.untoggle_optimizer(self.gen_optimizer)
 
-#         tonetz = tonetz.to(torch.float32).cuda()
-        
-#         # print(X.shape)
-#         # X = X.view(X.size(0), X.size(2), X.size(3))
+        # train discriminator
+        # Measure discriminator's ability to classify real from generated samples
+        self.toggle_optimizer(self.dis_optimizer)
 
-#         tonetz = tonetz.permute(0, 2, 1)
-#         print(tonetz.shape)
+        # how well can it label as real?
+        valid = torch.ones(imgs.size(0), 1)
+        valid = valid.type_as(imgs)
 
+        real_loss = self.loss_fn(self.discriminator(imgs), valid)
 
-#         h0 = torch.randn(1, tonetz.shape[0], 512).cuda()
+        # how well can it label as fake?
+        fake = torch.zeros(imgs.size(0), 1)
+        fake = fake.type_as(imgs)
 
-#         tonetz, hn = self.gru2(tonetz, h0)
+        fake_loss = self.loss_fn(self.discriminator(self(z).detach()), fake)
 
-#         tonetz_output = tonetz[:, -1, :]
-#         # print(cnn.shape)
-#         # print(output.shape)
-#         # Concatenate cnn and output along the last dimension
-#         combined = torch.cat((cnn, chroma_output, tonetz_output), dim=1)
-
-#         output = self.fc(combined)
-
-#         # print(output.shape)
-
-#         return output
+        # discriminator loss is the average of these
+        d_loss = (real_loss + fake_loss) / 2
+        self.log("d_loss", d_loss, prog_bar=True)
+        self.manual_backward(d_loss)
+        self.dis_optimizer.step()
+        self.dis_optimizer.zero_grad()
+        self.untoggle_optimizer(self.dis_optimizer)
     
-#     def configure_optimizers(self):
-#             scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.90)
-#             return {"optimizer": self.optimizer, "lr_scheduler": scheduler}
+    def on_validation_epoch_end(self):
+        z = self.validation_z.type_as(self.generator.model[0].weight)
 
-#     def _step(self, batch):
-#         x, y = batch
-#         preds = self(x)
-
-#         # y = y.squeeze()  # Remove singleton dimensions
-#         # print(y)
-#         # print(preds)
-
-#         loss = self.loss_fn(preds, y)
-#         # acc = self.acc(preds, y)
-#         acc_preds = torch.argmax(preds, dim=1)
-#         acc = self.acc(acc_preds, y)
-#         return loss, acc
-    
-#     def training_step(self, batch, batch_idx):
-#         loss, acc = self._step(batch)
-#         # perform logging
-#         self.log(
-#             "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-#         )
-#         self.log(
-#             "train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True
-#         )
-#         return loss
-    
-#     def validation_step(self, batch, batch_idx):
-#         loss, acc = self._step(batch)
-#         # perform logging
-#         self.log("val_loss", loss, on_epoch=True, prog_bar=False, logger=True)
-#         self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
-
-#     def test_step(self, batch, batch_idx):
-#         loss, acc = self._step(batch)
-#         # perform logging
-#         self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
-#         self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
+        # log sampled images
+        sample_imgs = self(z)
+        grid = torchvision.utils.make_grid(sample_imgs)
+        self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
