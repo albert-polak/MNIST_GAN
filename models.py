42a6cd089ab6d360578f78a5b5f3b24c4677c228
albert_polak <lugii1999@gmail.com>
2023-12-13 01:51:16 +0100

    training step refactoring

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ import lightning as L
 
 from torchmetrics import Accuracy  
 from torch.optim import Adam
+import torch.nn.functional as F
 
 class Discriminator(nn.Module):
     def __init__(self, img_channels, features_dim):
@@ -46,7 +47,7 @@ class Generator(nn.Module):
         self.features_dim = features_dim
 
         self.generator = nn.Sequential(
-            nn.ConvTranspose2d(z_dim, features_dim*16, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(z_dim, features_dim*16, kernel_size=4, stride=1, padding=0),
             nn.BatchNorm2d(features_dim*16),
             nn.ReLU(),
             nn.ConvTranspose2d(features_dim*16, features_dim*8, kernel_size=4, stride=2, padding=1),
@@ -75,13 +76,15 @@ class GAN(L.LightningModule):
         self.lr = lr
         self.batch_size = batch_size
 
-        self.loss_fn = nn.BCELoss()
+        # self.loss_fn = nn.BCELoss()
 
         self.generator = Generator(self.hparams.z_dim, img_channels=img_channels, features_dim=features_dim)
         self.discriminator = Discriminator(img_channels=img_channels, features_dim=features_dim)
 
         self.gen_optimizer = Adam(self.generator.parameters(), lr=self.lr)
         self.dis_optimizer = Adam(self.discriminator.parameters(), lr=self.lr)
+
+        self.validation_z = torch.randn(32, self.hparams.z_dim, 1, 1)
 
     def forward(self, X):
         return self.generator(X)
@@ -90,49 +93,51 @@ class GAN(L.LightningModule):
             # scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.90)
             return self.gen_optimizer, self.dis_optimizer
 
+    def adversarial_loss(self, y_hat, y):
+        # loss = nn.BCELoss()
+        return F.binary_cross_entropy(y_hat, y)
+
     def training_step(self, batch):
+        torch.set_grad_enabled(True)
         g_opt, d_opt = self.optimizers()
         imgs, _ = batch
 
         # sample noise
-        z = torch.randn(imgs.shape[0], self.hparams.latent_dim)
+        z = torch.randn(imgs.shape[0], self.hparams.z_dim, 1, 1)
+        # z.requires_grad_(True)
         z = z.type_as(imgs)
 
         # train generator
         # generate images
-        self.toggle_optimizer(g_opt)
-        generated_imgs = self(z)
+        
+        fake = self(z)
 
-        # log sampled images
-        sample_imgs = generated_imgs[:6]
-        grid = torchvision.utils.make_grid(sample_imgs)
-        self.logger.experiment.add_image("generated_images", grid, 0)
-
-        # ground truth result (ie: all fake)
-        # put on GPU because we created this tensor inside training_loop
-        real_label = torch.ones((imgs.size(0), 1), device=self.device)
-        fake_label = torch.zeros((imgs.size(0), 1), device=self.device)
-        real_label = real_label.type_as(imgs)
-        fake_label = fake_label.type_as(imgs)
+        # # log sampled images
+        # sample_imgs = fake[:6]
+        # grid = torchvision.utils.make_grid(sample_imgs)
+        # self.logger.experiment.add_image("generated_images", grid, 0)
 
         # adversarial loss is binary cross-entropy
-        g_loss = self.loss_fn(self.discriminator(generated_imgs), real_label)
+        output = self.discriminator(fake).reshape(-1)
+        g_loss = self.adversarial_loss(output, torch.ones_like(output))
         self.log("g_loss", g_loss, prog_bar=True)
         g_opt.zero_grad()
         self.manual_backward(g_loss)
         g_opt.step()
-        
-        self.untoggle_optimizer(g_opt)
+
+
+        disc_real = self.discriminator(imgs).reshape(-1)
+        disc_fake = self.discriminator(fake.detach()).reshape(-1)
+
 
         # train discriminator
         # Measure discriminator's ability to classify real from generated samples
-        self.toggle_optimizer(d_opt)
 
         # how well can it label as real?
-        real_loss = self.loss_fn(self.discriminator(imgs), real_label)
+        real_loss = self.adversarial_loss(disc_real, torch.ones_like(disc_real))
 
         # how well can it label as fake?
-        fake_loss = self.loss_fn(self.discriminator(self(z).detach()), fake_label)
+        fake_loss = self.adversarial_loss(disc_fake, torch.ones_like(disc_fake))
 
         # discriminator loss is the average of these
         d_loss = (real_loss + fake_loss) / 2
@@ -140,12 +145,10 @@ class GAN(L.LightningModule):
         d_opt.zero_grad()
         self.manual_backward(d_loss)
         d_opt.step()
-       
-        self.untoggle_optimizer(d_opt)
 
-    
-    def on_validation_epoch_end(self):
-        z = self.validation_z.type_as(self.generator.model[0].weight)
+
+    def on_epoch_end(self):
+        z = self.validation_z.type_as(self.generator.generator[0].weight)
 
         # log sampled images
         sample_imgs = self(z)
